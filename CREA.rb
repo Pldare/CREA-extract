76fc8a4fbb2760b47848d062c2feb8faef2521b8
Pldare <35677924+Pldare@users.noreply.github.com>
2019-02-15 22:23:21 +0800

    fix Compile problem

--- CREA.rb
+++ CREA.rb
@@ -1,6 +1,5 @@
 nnname=ARGV[0].to_s
-$file=File.open(nnname,"rb")
-i=$file.read(4)#pos=>4
+
 def r_fist(_o)
 	unk = r_long("V")#unkow
 	total_data = r_long("V")
@@ -9,7 +8,6 @@ def r_fist(_o)
 	files = ttt(r_long("V")) #get file quantity
 	unk2 = r_long("V")
 	#_ppp=File.new("tmp_list.txt","w")
-	_ppp=File.new("un.bms","a")
 	print "file table start:0x",$file.pos.to_s(16),"\n"
 	for nm in 1..files
 		offset = ttt(r_long("V"))#start
@@ -28,7 +26,6 @@ def r_fist(_o)
 		print "room in 0x",$file.pos.to_s(16),"\n"
 		_ppp.print "log \"",name,"\" 0x",offset.to_s(16)," 0x",fsize.to_s(16),"\n"
 	end
-	_ppp.close
 end
 def ttt(_a)
 	return _a.to_s.gsub("[","").gsub("]","").to_i
@@ -38,19 +35,25 @@ def mk_file(_off,_stop,_na)#offset,file size,make file name
 	_n_file=File.new(_na,"w")#make file
 	$file.seek(_off)
 	_tmp_r=$file.read(_stop)
-	_n_file.syswrite(_tmp_r)
+	_n_file.puts _tmp_r
 	$file.seek(_org_pos)
 	_n_file.close
 end
 def r_long(_p)
 	return $file.read(4).unpack(_p)
 end
-if i == "CRAE"
-	puts "CRAE file"
-	r_fist(0)
+if nnname.size > 0
+	$file=File.open(nnname,"rb")
+	i=$file.read(4)#pos=>4
+	if i == "CRAE"
+		puts "CRAE file"
+		r_fist(0)
+	else
+		print "this no CRAE file"
+		system "pause"
+	end
+	#use quickbms
+	system "quickbms.exe un.bms #{nnname}"
 else
-	print "this no CRAE file"
-	system "pause"
+	print "file?"
 end
-#use quickbms
-system "quickbms.exe un.bms #{nnname}"
